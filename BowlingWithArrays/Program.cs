e2c1161d2d936706af78b35d32318a32f28a7fa3
Michael Clemons <Mclemons@tql.com>
2024-06-14 10:31:53 -0400

    Add project files.

---
+++ BowlingWithArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BowlingWithArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] bowlingFrames = new int[10];
+            List<int> frames = new List<int>();
+            int score = 0;
+            int sumScore = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"Please enter a Score for frame {i}: ");
+                score = Convert.ToInt32(Console.ReadLine());
+                
+                if (score <= 30)
+                {
+                    frames.Add(score);
+                }
+                else
+                {
+                    Console.WriteLine("That score is not possible please enter a number between 0 and 30");
+                    i--;
+                }
+            }
+                foreach (int frame in frames)
+                {
+                    sumScore += frame;
+                } 
+                    Console.WriteLine($"Your total score is {sumScore}");            
+        }
+    }
+}
